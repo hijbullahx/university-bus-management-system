eaea042c570b6e84ccf3dfc64c1c6fba81da5764
hijbullahx <hijbullah119445@gmail.com>
2025-09-11 18:21:52 +0600

    logout route set

--- buses/urls.py
+++ buses/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views, views_admin
+from . import views_logout
 from . import views_admin_route, views_admin_route_edit
 
 app_name = 'buses' # Define the app namespace
 
 urlpatterns = [
+  path('logout/', views_logout.custom_logout, name='custom_logout'),
   path('', views.bus_schedule_list, name = 'bus_schedule_list'),
   path('admin-panel/', views_admin.admin_dashboard, name='custom_admin_dashboard'),
   path('admin-panel/create-route/', views_admin_route.create_route, name='custom_create_route'),
