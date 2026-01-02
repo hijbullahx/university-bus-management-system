3ede11924ee1282d0dc8b854f4563782732c6c42
hijbullahx <hijbullah119445@gmail.com>
2026-01-02 18:05:07 +0600

    Complete cleanup: remove old duplicate root-level Django files

--- buses/api_urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from .api_views import BusRouteViewSet, BusScheduleViewSet, GlobalSettingsViewSet
-
-router = routers.DefaultRouter()
-router.register(r'routes', BusRouteViewSet)
-router.register(r'schedules', BusScheduleViewSet)
-router.register(r'global-settings', GlobalSettingsViewSet)
-
-urlpatterns = router.urls
--- buses/views_admin_shuttle_delete.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from buses.models import BusRoute
-
-def delete_shuttle_route(request, route_id):
-    route = get_object_or_404(BusRoute, pk=route_id, is_shuttle=True)
-    if request.method == 'POST':
-        route.delete()
-        return redirect('buses:custom_admin_dashboard')
-    return render(request, 'buses/custom_admin/delete_route.html', {'route': route})
