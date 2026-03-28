220593f6d47a639d71c9a387470642d1f6995599
hijbullahx <hijbullah119445@gmail.com>
2026-03-28 08:49:17 +0600

    changes

--- core/settings.py
+++ core/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'notifications',
     'reports',
     'locations',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,13 @@ ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'admin@localhost')
 
 GPS_UPDATE_INTERVAL = 5
 ETA_CALCULATION_BUFFER = 1.2
+
+# Cloudinary storage settings (for free persistent media)
+if os.getenv('CLOUDINARY_URL'):
+    INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+        'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+        'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    }
